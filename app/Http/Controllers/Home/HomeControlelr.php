b76cf93916355057ba82becdecdde9bad675d41e
tunv <nguyenvantu191298@gmail.com>
2020-09-01 20:37:33 +0700

    change Public to Home cause error

--- app/Http/Controllers/Home/HomeControlelr.php
+++ app/Http/Controllers/Home/HomeControlelr.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers\Public;
+namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PublicController extends Controller
+class HomeControlelr extends Controller
 {
     //
 }
